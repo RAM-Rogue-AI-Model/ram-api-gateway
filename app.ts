e6dcae20fee3166c12f7ee2cc75786eff801a4fe
OlivierRecher <recher.olivier@outlook.fr>
2026-01-29 10:51:16 +0100

    feat(gateway#1): use dotenv

--- app.ts
+++ app.ts
@@ -1,13 +1,14 @@
 import express from 'express';
+import 'dotenv/config';
 
 const app = express();
-const port = 3001; //TODO: use .env
+const port = process.env.PORT || 3001;
 
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+    res.send('Hello API Gateway!');
 });
 
 app.listen(port, () => {
-  // eslint-disable-next-line no-console
-  console.log(`Server is running on port ${port}`);
+    // eslint-disable-next-line no-console
+    console.log(`Server is running on port ${port}`);
 });
