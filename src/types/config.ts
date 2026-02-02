b0d5c0a62eed4f2255abf53b6c09af736d208ffb
OlivierRecher <recher.olivier@outlook.fr>
2026-02-02 11:58:23 +0100

    feat(): env file with fake secret

--- src/types/config.ts
+++ src/types/config.ts
@@ -1,9 +1,14 @@
 interface configType {
   PORT: number;
   CLIENT_URL: string;
-  MS_USER_URL: string;
-  MS_PLAYER_URL: string;
+  MS_BATTLE_URL: string;
+  MS_EFFECT_URL: string;
+  MS_ENEMY_URL: string;
   MS_GAME_URL: string;
+  MS_ITEM_URL: string;
+  MS_LOGGER_URL: string;
+  MS_PLAYER_URL: string;
+  MS_USER_URL: string;
   SALT_ROUNDS: number;
   JWT_SECRET: string;
   INTERNAL_SECRET: string;
--- src/utils/config.ts
+++ src/utils/config.ts
@@ -7,11 +7,16 @@ dotenv.config();
 const config: configType = {
   PORT: Number(process.env.PORT ?? 3001),
   CLIENT_URL: process.env.CLIENT_URL ?? 'http://localhost:3000',
-  MS_USER_URL: process.env.MS_USER_URL ?? 'http://localhost:3002',
-  MS_PLAYER_URL: process.env.MS_PLAYER_URL ?? 'http://localhost:3008',
+  MS_BATTLE_URL: process.env.MS_BATTLE_URL ?? 'http://localhost:3002',
+  MS_EFFECT_URL: process.env.MS_EFFECT_URL ?? 'http://localhost:3003',
+  MS_ENEMY_URL: process.env.MS_ENEMY_URL ?? 'http://localhost:3004',
   MS_GAME_URL: process.env.MS_GAME_URL ?? 'http://localhost:3005',
+  MS_ITEM_URL: process.env.MS_ITEM_URL ?? 'http://localhost:3006',
+  MS_LOGGER_URL: process.env.MS_LOGGER_URL ?? 'http://localhost:3007',
+  MS_PLAYER_URL: process.env.MS_PLAYER_URL ?? 'http://localhost:3008',
+  MS_USER_URL: process.env.MS_USER_URL ?? 'http://localhost:3009',
 
-  SALT_ROUNDS: process.env.SALT_ROUNDS ? Number(process.env.SALT_ROUNDS) : 100,
+  SALT_ROUNDS: process.env.SALT_ROUNDS ? Number(process.env.SALT_ROUNDS) : 10,
   JWT_SECRET: process.env.JWT_SECRET ?? 'ram-user-secret-jwt-token',
   INTERNAL_SECRET: process.env.INTERNAL_SECRET ?? 'internal_secret',
 };
